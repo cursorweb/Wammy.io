a2c0f69dc118281df10759ba5dcc71742397cd0d
Junhao Zhang <junhaodev@gmail.com>
2021-02-17 17:46:21 -0600

    Updated players

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -2,14 +2,24 @@ import { values } from "../global";
 
 export function draw() {
   const { ctx, socket, player, tanks } = values;
+
   ctx.fillStyle = "gray";
   ctx.fillRect(0, 0, innerWidth, innerHeight);
 
+  ctx.save();
+  ctx.scale(1, 1); // todo: sight
+  ctx.translate(innerWidth / 2, innerHeight / 2);
   ctx.fillStyle = "blue";
   ctx.beginPath();
-  ctx.ellipse(player.x, player.y, 20, 20, 0, 0, Math.PI * 2);
+  ctx.ellipse(0, 0, 20, 20, 0, 0, Math.PI * 2);
   ctx.closePath();
   ctx.fill();
+  ctx.restore();
+
+  // game graphics
+  ctx.save();
+  ctx.translate(innerWidth / 2 - player.x, innerHeight / 2 - player.y);
+  ctx.scale(1, 1); // todo: sight
 
   for (const k in tanks) {
     const tank = tanks[k];
@@ -20,7 +30,21 @@ export function draw() {
     ctx.closePath();
     ctx.fill();
 
-    ctx.fillStyle = "black";
-    ctx.fillText(tank.name, tank.x, tank.y);
+    text(tank.name, tank.x, tank.y);
   }
+  ctx.restore();
+
+  // menu graphics (lb, score)
+}
+
+function text(s, x, y, color = "#fff", border = "#000", size = 12, center = true) {
+  const { ctx } = values;
+
+  ctx.font = `${size}px Ubuntu, sans-serif`;
+  if (center) ctx.textAlign = "center";
+  ctx.fillStyle = color;
+  ctx.strokeStyle = border;
+  ctx.lineWidth = 4;
+  ctx.strokeText(s, x, y);
+  ctx.fillText(s, x, y);
 }
